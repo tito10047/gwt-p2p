c4df158b87c934550f40d4e8ecb521265fc31c6c
mostka.j@gmail.com <mostka.j@gmail.com@f24c045f-2341-7ec8-5199-5db838d2eb80>
2013-11-04 11:32:50 +0000

    work on development mode

--- src/com/mostka/p2p/client/Gwt_p2p.java
+++ src/com/mostka/p2p/client/Gwt_p2p.java
@@ -1,13 +1,29 @@
 package com.mostka.p2p.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class Gwt_p2p implements EntryPoint {
 
 
+	private Demo1 demo1;
+
 	public void onModuleLoad() {
-		RootPanel.get().add(new HTML("Foo"));
+		init();
+		demo1 = new Demo1();
 	}
+	public native void init()/*-{
+		$wnd.logcnt=0;
+	}-*/;
+
+	public static native void log(String objects ) /*-{
+		console.log($wnd.logcnt+"----"+objects);
+		$wnd.logcnt++;
+	}-*/;
+	public static native void log(String str, JavaScriptObject objects ) /*-{
+	console.log($wnd.logcnt+"----"+str, objects);
+	$wnd.logcnt++;
+}-*/;
 }
